00b3ea6f61b64cb61c7a47dedf8e2c3c2ef4e5cd
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-03 14:02:08 +0000

    Update day1.py

--- answers/day1.py
+++ answers/day1.py
@@ -1,24 +1,28 @@
+from collections import Counter
 
 
-with open(r'data\day1.txt') as f:
-    data = f.readlines()
-
-first_list = []
-second_list = []
-
-
-for lines in data:
-    first, second = lines.split()
-    first_list.append(int(first))
-    second_list.append(int(second))
-
-first_list.sort()
-second_list.sort()
+def question_one(data):
+    first_list, second_list = zip(*(map(int, line.split()) for line in data))
+    answer = sum(abs(first - second) for first, second in zip(sorted(first_list), sorted(second_list)))
+    print(f'The answer to question one is: {answer}')
+    return first_list, second_list
 
 
-sum = 0
-for i, value in enumerate(first_list):
-    sec_val = second_list[i]
+def question_two(first_list, second_list):
+    similarity_score = 0
+    second_list_counter = Counter(second_list)
 
-    sum += ((sec_val - value)**2)**0.5
-print(sum)
+    for number, first_lst_cnt in Counter(first_list).items():
+        if sec_list_cnt := second_list_counter.get(number):
+            similarity_score += number * sec_list_cnt * first_lst_cnt
+
+    return similarity_score
+
+
+if __name__ == '__main__':
+    with open(r'data\day1.txt') as f:
+        data = f.readlines()
+
+    first_list, second_list = question_one(data)  # 1722302
+
+    score = question_two(first_list, second_list)  # 20373490
