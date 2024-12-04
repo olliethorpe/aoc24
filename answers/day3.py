afea0c4c22af73af9767f8433231f1562dba2543
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-04 16:07:50 +0000

    Update day3.py day3.txt

--- answers/day3.py
+++ answers/day3.py
@@ -1,11 +1,20 @@
 from utils import cache_and_read_input
+import re
 
 
 def question_one(data):
-    pass
+    total = 0
+    pattern = re.compile(r'mul\((\d+,\d+)\)')
+
+    for line in data:
+        for match in re.findall(pattern, line):
+            n1, n2 = map(int, match.split(','))
+            total += n1 * n2
+
+    return total
 
 
 if __name__ == "__main__":
     data = cache_and_read_input(3)
-    answer = question_one
+    answer = question_one(data)
     print(answer)
