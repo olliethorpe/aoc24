0afa2545d19630db5edb1c5b56c0b88d27548834
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-04 15:56:57 +0000

    Update day3.py

---
+++ answers/day3.py
@@ -0,0 +1,11 @@
+from utils import cache_and_read_input
+
+
+def question_one(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = cache_and_read_input(3)
+    answer = question_one
+    print(answer)
