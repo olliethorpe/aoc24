9da38cdb037aa1edc9afdcf4fac7439187795d4a
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-11 13:36:08 +0000

    Update day6.py day6.txt

--- answers/day6.py
+++ answers/day6.py
@@ -4,11 +4,10 @@ from utils import cache_and_read_input
 def question_one(data):
     next_dir = {(0, -1): (1, 0), (1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1)}
     guard_pos, direction = find_guard(data)
-    print(f'Guard found at {guard_pos} in direction {direction}')
     visited_positions = set()
+    visited_positions.add(guard_pos)
     while True:
         guard_pos = (guard_pos[0] + direction[0], guard_pos[1] + direction[1])
-        print(F'NEXT POS {guard_pos}')
         try:
             next_object = data[guard_pos[1]][guard_pos[0]]
             if guard_pos[0] < 0 or guard_pos[0] >= len(data[0]) or guard_pos[1] < 0 or guard_pos[1] >= len(data):
@@ -33,7 +32,47 @@ def find_guard(data):
                 return (x, y), guard[search]
 
 
+def find_loop(data):
+    next_dir = {(0, -1): (1, 0), (1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1)}
+    guard_pos, direction = find_guard(data)
+    record = set([(guard_pos, direction),])
+    while True:
+        guard_pos = (guard_pos[0] + direction[0], guard_pos[1] + direction[1])
+        try:
+            next_object = data[guard_pos[1]][guard_pos[0]]
+            if guard_pos[0] < 0 or guard_pos[0] >= len(data[0]) or guard_pos[1] < 0 or guard_pos[1] >= len(data):
+                return False
+        except:
+            return False
+
+        if next_object == '#':
+            guard_pos = (guard_pos[0] - direction[0], guard_pos[1] - direction[1])
+            direction = next_dir[direction]
+        else:
+            if (guard_pos, direction) in record:
+                return True
+            record.add((guard_pos, direction))
+
+
+def question_two(data):
+    total_loops = 0
+    for y in range(len(data)):
+        line = [x for x in data[y]]
+        for x in range(len(line)):
+            before = data[y][x]
+            if before in ['^']:
+                continue
+            line[x] = '#'
+            data[y] = ''.join(line)
+            if before != '#' and find_loop(data):
+                total_loops += 1
+            line[x] = before
+            data[y] = ''.join(line)
+    return total_loops
+
+
 if __name__ == "__main__":
     data = cache_and_read_input(6)
-    answer = question_one(data)
-    print(len(answer))
+    data = [line.replace('\n', '') for line in data]
+    answer = question_two(data)
+    print(answer)
