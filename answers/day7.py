262f2842c276db646c3367208974523e9a8a7a0a
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-11 16:32:08 +0000

    Update day7.py day7.txt

--- answers/day7.py
+++ answers/day7.py
@@ -23,7 +23,27 @@ def question_one(data):
     return sum_of_correct_tests
 
 
+def question_two(data):
+    sum_of_correct_tests = 0
+    pattern = re.compile(r'(\d+):([\d\s]+)')
+    operators = ['*', '+', '||']
+    for row in data:
+        match = re.match(pattern, row)
+        result = int(match[1])
+        numbers = match[2].split()
+        combs = itertools.product(operators, repeat=len(numbers)-1)
+        for comb in combs:
+            total = int(numbers[0])
+            for num, op in zip(numbers[1:], comb):
+                total = eval(f"{total} {op} {num}")
+            if total == result:
+                sum_of_correct_tests += result
+                break
+
+    return sum_of_correct_tests
+
+
 if __name__ == "__main__":
     data = cache_and_read_input(7)
-    answer = question_one(data)
+    answer = question_two(data)
     print(answer)
