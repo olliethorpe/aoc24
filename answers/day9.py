7468905be1ee2d44ba6ada7d800fe83bf0b473b3
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-16 19:00:20 +0000

    Update day9.py

---
+++ answers/day9.py
@@ -0,0 +1,11 @@
+from utils import cache_and_read_input
+
+
+def question_one(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = cache_and_read_input(9)
+    answer = question_one(data)
+    print(len(answer))
