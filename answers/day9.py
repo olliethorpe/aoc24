4226ae119d5e0614c263a2e31508229f059f7e64
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-12-22 19:45:49 +0000

    Update day9.py aligned.txt

--- answers/day9.py
+++ answers/day9.py
@@ -2,26 +2,23 @@ from utils import cache_and_read_input
 
 
 def question_one(data):
-    blocks = [b for b in calculate_blocks(data)]
-    print(''.join(blocks))
-    blocks_b = blocks[::-1]
-    for i in range(len(blocks_b) - 1):
-        block = blocks_b[i]
+    blocks = calculate_blocks(data)
+    blocks_backwards = blocks[::-1]
+    for i in range(len(blocks_backwards) - 1):
+        block = blocks_backwards[i]
         if block != '.':
             next_empty_space = find_empty_space(blocks)
             if next_empty_space > (len(blocks) - 1) - i:
                 break
             blocks[next_empty_space] = block
             blocks[(len(blocks) - 1) - i] = '.'
-    print(''.join(blocks))
     sum = 0
     position = 0
     while blocks[position] != '.':
         sum += position * int(blocks[position])
         position += 1
     return sum
-#88935968966
-#88935968966
+
 
 def find_empty_space(blocks):
     for i in range(len(blocks) - 1):
@@ -31,19 +28,18 @@ def find_empty_space(blocks):
 
 
 def calculate_blocks(data):
-    a = ''
+    a = []
     for i, block in enumerate(data[0]):
         if i % 2 == 0:
-            id = int(i / 2)
             for _ in range(int(block)):
-                a += str(id)
+                a.append(int(i / 2))
         else:
             for _ in range(int(block)):
-                a += '.'
+                a.append('.')
     return a
 
 
 if __name__ == "__main__":
     data = cache_and_read_input(9)
-    answer = question_one(data)
-    print(answer)
+    a = question_one(data)
+    print(a)
